ffae876cf44fe1090d1a4c7da4e28a40590647e7
suvin <subin110900@naver.com>
2025-11-17 13:48:38 +0900

    ㄴㄴ
    ㄴㄴ

---
+++ hotelServer/src/main/java/hms/hotelserver/HotelServer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hms.hotelserver;
+
+/**
+ *
+ * @author subin
+ */
+public class HotelServer {
+
+    public static void main(String[] args) {
+        System.out.println("안녕하요!");
+    }
+}
