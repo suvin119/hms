dafb05d79276559d7f597952457d0b4de087029b
lgi0507 <lgi0507@naver.com>
2025-11-26 19:40:41 +0900

    코드 수정
    기본값으로 들어있던 "관리자 홍길동"을 없애고, 처음에는 비어있도록(null) 초기화

--- hotelClient/src/main/java/main/Session.java
+++ hotelClient/src/main/java/main/Session.java
@@ -1,10 +1,9 @@
 package main;
 
 public class Session {
-    // 현재 프로그램에 로그인한 사용자
-    // 테스트를 위해 기본값을 '관리자'로 설정해둠 (나중에 로그인 화면에서 바꿔줘야 함)
-    public static User currentUser = new Admin("관리자홍길동", "admin01");
+    //처음엔 로그인 안 된 상태로 변경
+    public static User currentUser = null;
     
-    // 테스트용: 직로 로그인하고 싶으면 주석 풀고 위를 주석 처리
+    // 테스트용: 직원으로 로그인하고 싶으면 주석 풀고 위를 주석 처리
     // public static User currentUser = new User("일반직원", "emp01");
 }
